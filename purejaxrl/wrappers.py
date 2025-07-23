b58cede21bb1f0f3e3d2b0251f17137bf4cff23e
Oliver Diamond <odiamond@wesleyan.edu>
2025-07-23 00:25:42 -0600

    fixed handling of truncation vs termination, truncated timesteps are now discarded

--- purejaxrl/wrappers.py
+++ purejaxrl/wrappers.py
@@ -114,6 +114,65 @@ class LogWrapper(GymnaxWrapper):
         return obs, state, reward, done, info
 
 
+@struct.dataclass
+class MultiTaskLogEnvState:
+    env_state: environment.EnvState
+    episode_returns: float
+    episode_lengths: int
+    returned_episode_returns: float
+    returned_episode_lengths: int
+    returned_episode_tasks: int
+    timestep: int
+
+
+class MultiTaskLogWrapper(GymnaxWrapper):
+    """Log the episode returns and lengths."""
+
+    def __init__(self, env: environment.Environment):
+        super().__init__(env)
+
+    @partial(jax.jit, static_argnums=(0,))
+    def reset(
+        self, key: chex.PRNGKey, params: Optional[environment.EnvParams] = None
+    ) -> Tuple[chex.Array, environment.EnvState]:
+        obs, env_state = self._env.reset(key, params)
+        state = MultiTaskLogEnvState(env_state, 0, 0, 0, 0, env_state.task, 0)
+        return obs, state
+
+    @partial(jax.jit, static_argnums=(0,))
+    def step(
+        self,
+        key: chex.PRNGKey,
+        state: environment.EnvState,
+        action: Union[int, float],
+        params: Optional[environment.EnvParams] = None,
+    ) -> Tuple[chex.Array, environment.EnvState, float, bool, dict]:
+        obs, env_state, reward, done, info = self._env.step(
+            key, state.env_state, action, params
+        )
+        new_episode_return = state.episode_returns + reward
+        new_episode_length = state.episode_lengths + 1
+        state = MultiTaskLogEnvState(
+            env_state=env_state,
+            episode_returns=new_episode_return * (1 - done),
+            episode_lengths=new_episode_length * (1 - done),
+            returned_episode_returns=state.returned_episode_returns * (1 - done)
+            + new_episode_return * done,
+            returned_episode_lengths=state.returned_episode_lengths * (1 - done)
+            + new_episode_length * done,
+            returned_episode_tasks=state.returned_episode_tasks * (1 - done)
+            + env_state.task * done,
+            timestep=state.timestep + 1,
+        )
+        info["returned_episode_returns"] = state.returned_episode_returns
+        info["returned_episode_lengths"] = state.returned_episode_lengths
+        info["timestep"] = state.timestep
+        info["returned_episode"] = done
+        info['returned_episode_tasks'] = state.returned_episode_tasks
+        
+        return obs, state, reward, done, info
+
+
 class BraxGymnaxWrapper:
     def __init__(self, env_name, backend="positional"):
         env = envs.get_environment(env_name=env_name, backend=backend)
